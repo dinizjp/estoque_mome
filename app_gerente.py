7eefdaba5d1e8e47d43c53aa1b515cd2b32fbe02
dinizjp <joopedrodiniz@gmail.com>
2025-03-11 17:14:14 -0300

    ajuste supabase

--- app_gerente.py
+++ app_gerente.py
@@ -40,7 +40,7 @@ def get_produtos():
     with get_db_connection() as conn:
         with conn.cursor() as cursor:
             cursor.execute("""
-                SELECT id, nome, categoria, unidade_medida, valor,  
+                SELECT id, nome, categoria, unidade_medida, valor  
                 FROM produtos 
                 ORDER BY nome
             """)
