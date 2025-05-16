c0f2f3b15820152786fb27e02ffcf0f297cedd48
dinizjp <joopedrodiniz@gmail.com>
2025-05-16 14:23:06 -0300

    ajustes

--- utils.py
+++ utils.py
@@ -38,7 +38,7 @@ def get_produtos():
     with get_db_connection() as conn:
         with conn.cursor() as cursor:
             cursor.execute("""
-                SELECT id, nome, categoria, valor  
+                SELECT id, nome, categoria  
                 FROM produtos 
                 ORDER BY nome
             """)
